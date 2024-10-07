e672feea87419bb2203a798136ad3ffdbaba15e9
Mishti Kesarwani <127041314+Mishtilorem@users.noreply.github.com>
2024-10-07 16:12:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["http://localhost:3000", "http://localhost:3001","https://task-manager-simplfied.netlify.app"],
+      origin: ["http://localhost:3000", "http://localhost:3001"],
       methods: ["GET", "POST", "DELETE", "PUT"],
       credentials: true,
     })
